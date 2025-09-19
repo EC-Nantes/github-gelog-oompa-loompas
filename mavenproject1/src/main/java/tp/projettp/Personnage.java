15a18719a120e365771f238b72496d187bb69057
nathan <nathan@hp-nathan>
2025-09-19 16:32:12 +0200

    création du fichier de la classe Personnage

---
+++ mavenproject1/src/main/java/tp/projettp/Personnage.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.projettp;
+
+/**
+ *
+ * @author nathan
+ */
+public class Personnage {
+    private String nom;
+    private int ptVie;
+    private int ptPar;
+}
