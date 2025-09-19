579b5867e283f65c44010722b8250f53c4571fac
nathan <nathan@hp-nathan>
2025-09-19 17:02:02 +0200

    Ajout des attribut et méthodes de Personnage

--- mavenproject1/src/main/java/tp/projettp/Personnage.java
+++ mavenproject1/src/main/java/tp/projettp/Personnage.java
@@ -10,6 +10,115 @@ package tp.projettp;
  */
 public class Personnage {
     private String nom;
+
+    public int getPtVie() {
+        return ptVie;
+    }
+
+    public void setPtVie(int ptVie) {
+        this.ptVie = ptVie;
+    }
+
+    public int getPtPar() {
+        return ptPar;
+    }
+
+    public void setPtPar(int ptPar) {
+        this.ptPar = ptPar;
+    }
+
+    public int getDegAtt() {
+        return degAtt;
+    }
+
+    public void setDegAtt(int degAtt) {
+        this.degAtt = degAtt;
+    }
+
+    public int getPageAtt() {
+        return pageAtt;
+    }
+
+    public void setPageAtt(int pageAtt) {
+        this.pageAtt = pageAtt;
+    }
+
+    public int getPagePar() {
+        return pagePar;
+    }
+
+    public void setPagePar(int pagePar) {
+        this.pagePar = pagePar;
+    }
+
+    public int getDistAttMax() {
+        return distAttMax;
+    }
+
+    public void setDistAttMax(int distAttMax) {
+        this.distAttMax = distAttMax;
+    }
+
+    public Point2D getPos() {
+        return pos;
+    }
+
+    public void setPos(Point2D pos) {
+        this.pos = pos;
+    }
     private int ptVie;
     private int ptPar;
+    private int degAtt;
+    private int pageAtt;
+    private int pagePar;
+    private int distAttMax;
+    private Point2D pos;
+    
+    public Personnage(String nom, int ptVie, int ptPar,int pageAtt, int pagePar, int distAttMax, Point2D pos){
+        this.nom=nom;
+        this.ptVie=ptVie;
+        this.ptPar=ptPar;
+        this.pageAtt=pageAtt;
+        this.pagePar=pagePar;
+        this.distAttMax=distAttMax;
+        this.pos=pos;
+    }
+    
+    public Personnage(Personnage perso){
+        this.nom=perso.nom;
+        this.ptVie=perso.ptVie;
+        this.ptPar=perso.ptPar;
+        this.pageAtt=perso.pageAtt;
+        this.pagePar=perso.pagePar;
+        this.distAttMax=perso.distAttMax;
+        this.pos=new Point2D(perso.pos);
+    }
+    public Personnage(){
+        this.nom="Lui";
+        this.ptVie=1;
+        this.ptPar=1;
+        this.pageAtt=1;
+        this.pagePar=1;
+        this.distAttMax=1;
+        this.pos=null;}
+    
+    public String getNom(){
+        if (this!=null){
+        return this.nom;}
+        else{return "NULL";}
+    }
+    
+    public boolean setNom(String n){
+        if (this!=null){
+        this.nom = n;
+        return true;}
+        else{return false;}
+    }
+    
+    public void deplace(){
+        }
+    
+    public void affiche(){
+    }
+    
 }
