2d8e4207a1a86486b6c9b8a0a20996f3f235376a
julda <julda@130.66.212.138>
2025-09-26 17:59:29 +0200

    création créature à partir de monstre

--- mavenproject1/src/main/java/tp/projettp/Personnage.java
+++ mavenproject1/src/main/java/tp/projettp/Personnage.java
@@ -9,8 +9,12 @@ package tp.projettp;
  * @author nathan
  */
 public class Personnage extends Creature{
-    public Personnage(int pV,int dA,int paAtt,int paPar,Point2D p){
+    int distAttMax;
+    int ptPar;
+    public Personnage(string nom,int pV,int pPar, int dA,int paAtt,int paPar,int dAtt, Point2D p){
         super(pV,dA,paAtt,paPar,p);
+        this.distAttMax=dAtt;
+        this.ptPar=pPar;
     }
     public Personnage(Personnage m){
         super(m.getPtVie(),m.getDegAtt(),m.getPageAtt(),m.getPagePar(),new Point2D(m.getPos()));
