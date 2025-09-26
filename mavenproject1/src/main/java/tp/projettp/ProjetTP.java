8499e40056c01a3764942dec43d7bbbbfa002fdb
julda <julda@130.66.212.138>
2025-09-26 15:21:12 +0200

    Merge origin/main into main
    # Conflicts:
    #	mavenproject1/src/main/java/tp/projettp/Point2D.java

--- mavenproject1/src/main/java/tp/projettp/ProjetTP.java
+++ mavenproject1/src/main/java/tp/projettp/ProjetTP.java
@@ -13,12 +13,12 @@ public class ProjetTP {
     public static void main(String[] args) {
         World map = new World();
         map.creerMondeAlea();
-        System.out.println("Robin fait 2 deplacements, voici ses 3 positions");
-        map.robin.affiche();
-        map.robin.deplace();
-        map.robin.affiche();
-        map.robin.deplace();
-        map.robin.affiche();
+        System.out.println("bugs fait 2 deplacements, voici ses 3 positions");
+        map.bugs.affiche();
+        map.bugs.deplace();
+        map.bugs.affiche();
+        map.bugs.deplace();
+        map.bugs.affiche();
         
     }
 }
