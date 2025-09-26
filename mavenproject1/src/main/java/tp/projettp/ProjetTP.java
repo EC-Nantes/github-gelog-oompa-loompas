5f1173eddf4234a59924b9902a88e9f74a5267e2
julda <julda@130.66.212.138>
2025-09-26 16:35:32 +0200

    javadoc

--- mavenproject1/src/main/java/tp/projettp/ProjetTP.java
+++ mavenproject1/src/main/java/tp/projettp/ProjetTP.java
@@ -13,12 +13,10 @@ public class ProjetTP {
     public static void main(String[] args) {
         World map = new World();
         map.creerMondeAlea();
-        System.out.println("bugs fait 2 deplacements, voici ses 3 positions");
-        map.bugs.affiche();
-        map.bugs.deplace();
-        map.bugs.affiche();
-        map.bugs.deplace();
-        map.bugs.affiche();
+        map.guillaumeT= new Archer(map.robin);
+        map.robin.deplace();
+        map.robin.affiche();
+        map.guillaumeT.affiche();
         
     }
 }
