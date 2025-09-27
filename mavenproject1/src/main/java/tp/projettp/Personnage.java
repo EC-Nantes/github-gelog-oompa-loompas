35ea1025c0f1bb9340191730cea8297cd4e5caf7
julda <julda@130.66.212.138>
2025-09-27 06:24:44 +0200

    rectification Créature et sous classes

--- mavenproject1/src/main/java/tp/projettp/Personnage.java
+++ mavenproject1/src/main/java/tp/projettp/Personnage.java
@@ -11,12 +11,36 @@ package tp.projettp;
 public class Personnage extends Creature{
     int distAttMax;
     int ptPar;
-    public Personnage(string nom,int pV,int pPar, int dA,int paAtt,int paPar,int dAtt, Point2D p){
-        super(pV,dA,paAtt,paPar,p);
+    public Personnage(String nom,int pV,int pPar, int dA,int paAtt,int paPar,int dAtt, Point2D p){
+        super(nom,pV,dA,paAtt,paPar,p);
         this.distAttMax=dAtt;
         this.ptPar=pPar;
     }
     public Personnage(Personnage m){
-        super(m.getPtVie(),m.getDegAtt(),m.getPageAtt(),m.getPagePar(),new Point2D(m.getPos()));
+        super(m.getNom(),m.getPtVie(),m.getDegAtt(),m.getPageAtt(),m.getPagePar(),new Point2D(m.getPos()));
+        this.distAttMax=m.getDistAttMax();
+        this.ptPar=m.getPtPar();
     }
+    public Personnage(){
+        super();
+        distAttMax=5;
+        ptPar=5;
+    }
+
+    public int getDistAttMax() {
+        return distAttMax;
+    }
+
+    public int getPtPar() {
+        return ptPar;
+    }
+
+    public void setDistAttMax(int distAttMax) {
+        this.distAttMax = distAttMax;
+    }
+
+    public void setPtPar(int ptPar) {
+        this.ptPar = ptPar;
+    }
+    
 }
