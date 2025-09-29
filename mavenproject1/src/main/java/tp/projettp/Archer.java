cabf76fc3abc8a7065e763e5f8635adee3dba569
julda <julda@130.66.212.138>
2025-09-29 13:47:40 +0200

    rectification Créature et sous classes

--- mavenproject1/src/main/java/tp/projettp/Archer.java
+++ mavenproject1/src/main/java/tp/projettp/Archer.java
@@ -4,6 +4,8 @@
  */
 package tp.projettp;
 
+import java.util.Random;
+
 /**
  *
  * @author nathan
@@ -24,12 +26,47 @@ public class Archer extends Personnage {
     }
     public Archer(){
         super();
-        nbFleches=100;
+        nbFleches=15;
     }
     
     public Archer( Archer a){
         super(a.getNom(), a.getPtVie(), a.getDegAtt(), a.getPtPar(),a.getPageAtt(), a.getPagePar(), a.getDistAttMax(), new Point2D(a.getPos()));
         this.nbFleches=a.getNbFleches();
     }
-    
+    public void combattre(Creature c){
+        if (this.getDistAttMax()==1){
+            Random tirage=new Random();
+            int Rand=tirage.nextInt(100)+1;
+            if(Rand<=this.getPageAtt()){
+                int Rand2=tirage.nextInt(100)+1;
+                if(Rand2>c.getPagePar()){
+                    c.loosePV(this.getDegAtt());
+                }
+                else{
+                    c.loosePV(this.getDegAtt()-c.getPtPar());
+                }
+                
+            }
+            else{
+                System.out.println("Attaque Ratée");
+            }
+        }
+        else if (this.getDistAttMax()==1){//A faire
+            Random tirage=new Random();
+            int Rand=tirage.nextInt(100)+1;
+            if(Rand<=this.getPageAtt()){
+                int Rand2=tirage.nextInt(100)+1;
+                if(Rand2>c.getPagePar()){
+                    c.loosePV(this.getDegAtt());
+                }
+                else{
+                    c.loosePV(this.getDegAtt()-c.getPtPar());
+                }
+                
+            }
+            else{
+                System.out.println("Attaque Ratée");
+            }
+        }
+    }
 }
