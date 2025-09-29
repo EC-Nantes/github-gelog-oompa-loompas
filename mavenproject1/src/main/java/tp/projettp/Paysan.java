e3fe166f7d7a75575e4a73e2b8781954762e8a18
julda <julda@130.66.212.138>
2025-09-29 09:39:33 +0200

    rectification Créature et sous classes

--- mavenproject1/src/main/java/tp/projettp/Paysan.java
+++ mavenproject1/src/main/java/tp/projettp/Paysan.java
@@ -9,14 +9,16 @@ package tp.projettp;
  * @author nathan
  */
 public class Paysan extends Personnage {
-    public Paysan(String nom, int ptVie, int degAtt,int ptPar,int pageAtt, int pagePar, int distAttMax, Point2D pos){
-        super( nom,  ptVie,  ptPar, pageAtt,  pagePar,  distAttMax,  pos);
+
+    public Paysan(String nom, int pV, int pPar, int degA, int paAtt, int paPar, int dAtt, Point2D p) {
+        super(nom, pV, pPar, degA, paAtt, paPar, dAtt, p);
     }
-    public Paysan(){
-        super();
+
+    public Paysan(Personnage p) {
+        super(p.getNom(),p.getPtVie(),p.getPagePar(),p.getDegAtt(),p.getPageAtt(),p.getPagePar(),p.getDistAttMax(), new Point2D(p.getPos()));
+    }
+
+    public Paysan() {
     }
     
-    public Paysan( Paysan p){
-        super(p.getNom(), p.getPtVie(), p.getPtPar(),p.getPageAtt(), p.getPagePar(), p.getDistAttMax(), p.getPos());
-    }
 }
