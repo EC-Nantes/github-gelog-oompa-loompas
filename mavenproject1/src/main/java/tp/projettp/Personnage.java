7d31784400163b7c30a36ce725a66bd47ae94069
julda <julda@130.66.212.138>
2025-10-13 16:42:52 +0200

    Destruction joueur attaquant

--- mavenproject1/src/main/java/tp/projettp/Personnage.java
+++ mavenproject1/src/main/java/tp/projettp/Personnage.java
@@ -9,36 +9,4 @@ package tp.projettp;
  * @author nathan
  */
 public abstract class Personnage extends Creature{
-    protected int degAtt;
-    protected int pageAtt;
-    protected int distAMax;
-
-    public int getDegAtt() {
-        return degAtt;
-    }
-
-    public int getPageAtt() {
-        return pageAtt;
-    }
-
-    public int getDistAMax() {
-        return distAMax;
-    }
-
-    public void setDegAtt(int degAtt) {
-        this.degAtt = degAtt;
-    }
-
-    public void setPageAtt(int pageAtt) {
-        this.pageAtt = pageAtt;
-    }
-
-    public void setDistAMax(int distAMax) {
-        this.distAMax = distAMax;
-    }
-    
-    public void parler(String discours){
-        System.out.println(this.getNom()+" : '"+discours+"'");
-    }
-
 }
