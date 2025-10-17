bc7f8861c471091b2201b6bde03186e57ff017f5
julda <julda@130.66.212.138>
2025-10-17 13:18:20 +0200

    Les créatures peuvent utiliser une nourriture

--- mavenproject1/src/main/java/tp/projettp/Nourriture.java
+++ mavenproject1/src/main/java/tp/projettp/Nourriture.java
@@ -15,38 +15,153 @@ public class Nourriture extends Objet implements Utilisable{
     private boolean bonus;
     private int valeur;
     private int residu;
+    private boolean creaturisable;
 
-    public int getDuree() {
-        return duree;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public int getEffet() {
-        return effet;
-    }
-
-    public boolean isBonus() {
-        return bonus;
-    }
-
-    public int getValeur() {
-        return valeur;
-    }
-
-    public int getResidu() {
-        return residu;
-    }
-    
-    public Nourriture(String nom,int effet,boolean estBonus,int valeurEffet, int duration){
+    /**
+     * création nourriture consaommable uniquement par les joueurs
+     * @param nom
+     * @param effet
+     * @param valeurEffet
+     * @param estBonus
+     * @param duration
+     */
+    public Nourriture(String nom,int effet,int valeurEffet,boolean estBonus, int duration){
         this.duree=duration;
         this.nom=nom;
         this.effet=effet;
         this.bonus=estBonus;
         this.valeur=valeurEffet;
         this.residu=0;
+        this.creaturisable=false;
+    }
+
+    /**
+     * création d'une nourriture applicable à tous
+     * @param nom
+     * @param effet
+     * @param valeurEffet
+     * @param estBonus
+     */
+    public Nourriture(String nom,int effet,int valeurEffet,boolean estBonus){
+        this.duree=1;
+        this.nom=nom;
+        this.effet=effet;
+        this.bonus=estBonus;
+        this.valeur=valeurEffet;
+        this.residu=0;
+        this.creaturisable=true;
+    }
+    
+    /**
+     *
+     * @param duree
+     */
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    /**
+     *
+     * @param nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     *
+     * @param effet
+     */
+    public void setEffet(int effet) {
+        this.effet = effet;
+    }
+
+    /**
+     *
+     * @param bonus
+     */
+    public void setBonus(boolean bonus) {
+        this.bonus = bonus;
+    }
+
+    /**
+     *
+     * @param valeur
+     */
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+    /**
+     *
+     * @param residu
+     */
+    public void setResidu(int residu) {
+        this.residu = residu;
+    }
+
+    /**
+     *
+     * @param creaturalisable
+     */
+    public void setCreaturisable(boolean creaturalisable) {
+        this.creaturisable = creaturalisable;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isCreaturisable() {
+        return creaturisable;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getDuree() {
+        return duree;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getEffet() {
+        return effet;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isBonus() {
+        return bonus;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getValeur() {
+        return valeur;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getResidu() {
+        return residu;
     }
     private int minmax(int actuel){
         int retour;
@@ -61,36 +176,43 @@ public class Nourriture extends Objet implements Utilisable{
         }
         return retour;
     }
-    public void utilisation(Personnage p){
+
+    /**
+     *
+     * @param c
+     */
+    public void utilisation(Creature c){
         //1 = distance att,pageAtt,pagePar,degatt,ptpar,
-        int nouveauSet;
-        if(!bonus){
-            valeur=-valeur;
-        }
-        if (effet==1){
-            if (p instanceof Archer){
-                nouveauSet=Math.max(valeur,0);
-                p.setDistAMax(nouveauSet);
+        if (creaturisable){
+            int nouveauSet;
+            if(!bonus){
+                valeur=-valeur;
             }
-        }
-        if (effet==2){
-            nouveauSet=minmax(p.getPagePar());
-            p.setPagePar(nouveauSet);
-        }
-        if (effet==3){
-            nouveauSet=minmax(p.getPageAtt());
-            p.setPageAtt(nouveauSet);
-        }
-        if (effet==4){
-            nouveauSet=Math.max(p.getDegAtt()+valeur,0);
-            p.setDegAtt(nouveauSet);
-        }
-        if (effet==5){
-            nouveauSet=Math.max(p.getPtPar()+valeur,0);
-            p.setPtPar(nouveauSet);
-        }
-        if(!bonus){
-            valeur=-valeur;
+            if (effet==1){
+                if (c instanceof Archer){
+                    nouveauSet=Math.max(valeur,0);
+                    c.setDistAMax(nouveauSet);
+                }
+            }
+            if (effet==2){
+                nouveauSet=minmax(c.getPagePar());
+                c.setPagePar(nouveauSet);
+            }
+            if (effet==3){
+                nouveauSet=minmax(c.getPageAtt());
+                c.setPageAtt(nouveauSet);
+            }
+            if (effet==4){
+                nouveauSet=Math.max(c.getDegAtt()+valeur,0);
+                c.setDegAtt(nouveauSet);
+            }
+            if (effet==5){
+                nouveauSet=Math.max(c.getPtPar()+valeur,0);
+                c.setPtPar(nouveauSet);
+            }
+            if(!bonus){
+                valeur=-valeur;
+            }
         }
     }
 }
