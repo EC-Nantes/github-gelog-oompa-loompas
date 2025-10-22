5d9216e5fd9c7cd661156e080b12a9f1a6ddef96
julda <julda@130.66.212.138>
2025-10-22 15:43:46 +0200

    javadoc

--- mavenproject1/src/main/java/tp/projettp/ElementDeJeu.java
+++ mavenproject1/src/main/java/tp/projettp/ElementDeJeu.java
@@ -9,40 +9,78 @@ package tp.projettp;
  * @author julda
  */
 public abstract class ElementDeJeu {
+
+    /**
+     *position de l'élément sur le plateau
+     */
     protected Point2D pos;
     private boolean estSolide;
+
+    /**
+     * nom de l'élément
+     */
     protected String nom;
     
-    
+    /**
+     * Constructeur originel utilisé pour les sous-classes
+     */
     public ElementDeJeu(){
         pos=new Point2D();
         estSolide=true;
         nom = "Sans nom 1";
     }
     
+    /**
+     *
+     * @return
+     */
     public Point2D getPos() {
         return this.pos;
     }
 
+    /**
+     *
+     * @return peut-on se déplacer sur l'élément ?
+     */
     public boolean isSolide() {
         return estSolide;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     *
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
     
+    /**
+     *
+     * @param pos
+     */
     public void setPos(Point2D pos) {
         this.pos = pos;
     }
 
+    /**
+     *
+     * @param estSolide
+     */
     public void setSolide(boolean estSolide) {
         this.estSolide = estSolide;
     }
+
+    /**
+     * affiche la position et le nom de l'élément dans le terminal
+     */
     public void affiche(){
         System.out.println("position de "+this.getNom()+" : ["+this.pos.getX()+","+this.pos.getY()+"]");
     }
